11caf300ce003638a740482c3ad1141da3c47124
victorroddev <javier.rod.dev@gmail.com>
2024-11-13 11:58:45 -0700

    Basic function

--- app.py
+++ app.py
@@ -1,13 +1,50 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+client = MongoClient('mongodb+srv://victordev:jcYVg21SmxpZMe@cluster0.lg82a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+db = client["ventas"]
+collection = db["ventas diarias"]
 
 
+#rutas para el formulario de registro de ventas:
+@app.route('/', methods=['GET', 'POST'])
+def sale_register():
+    """Function for register the sales"""
+    if request.method == 'POST':
+        quantity = int(request.form['quantity'])
+        price = float(request.form['price'])
+        date = datetime.now()
 
-if __name__ == '__main__' :
-    app.run(debug=True, port=4000)
+        sale = {
+            'quantity' : quantity,
+            'price': price,
+            "date": date
+        }
+        collection.insert_one(sale)
 
+        return "Venta Registrada exitosamente"
+    
+    return render_template('form_sale.html')
+
+#Ruta para mostrar el resumen diario
+@app.route('/resume')
+def daily_resume():
+    """Pipeline for concate the sales by day"""
+    pipeline = [
+        {
+            "$group": {
+                "_id": {"$dateToString": {"format": "%Y-%m-%d", "date": "$date"}},
+                "total_quantity": {"$sum": "$quantity"},
+                "total_earnings": {"$sum": "$price"}
+            }
+        }
+    ]
+    results = list(collection.aggregate(pipeline))
+
+    return render_template('resume.html', results=results)
+
+if __name__ == '__main__':
+    app.run(debug=True)
